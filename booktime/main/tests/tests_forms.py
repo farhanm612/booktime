04ba4f739025aeef381dcbe38af95355800da95d
Farhan Mahmood <farhanm612@gmail.com>
2019-05-14 15:13:24 +0500

    Added models, signals and tests

--- booktime/main/tests/tests_forms.py
+++ booktime/main/tests/tests_forms.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.core import mail
+from django.urls import reverse
 from main import forms
 
 
@@ -23,6 +24,6 @@ class TestForm(TestCase):
     def test_contact_us_page_works(self):
         response = self.client.get(reverse("contact-us"))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "main/contact_form.html")
+        self.assertTemplateUsed(response, 'contact_form.html')
         self.assertContains(response, 'BookTime')
         self.assertIsInstance(response.context['form'], forms.ContactForm)
